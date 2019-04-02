dea6125a890af34d3d4944ae3d8c164f96bf3cdf
kmietotwy <kmietotwy@gmail.com>
2019-04-01 18:48:46 -1000

    created vault

--- vault.js
+++ vault.js
@@ -1,4 +1,26 @@
 'use strict';
 module.exports = function() {
-  
+  let vaultObj = {};
+
+  function setValue(key, value){
+    if(!key || !value){
+      return null;
+    }
+    
+    vaultObj.key = value;
+    return value;
+  }
+
+  function getValue(key){
+    if(!vaultObj.key){
+      return null;
+    } else {
+      return vaultObj.key;
+    }
+  }
+
+  return {
+    setValue: setValue,
+    getValue: getValue
+  }
 };
